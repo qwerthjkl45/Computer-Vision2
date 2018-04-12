9b81acd1836b43fd92bafeede62d88e5def38e1b
qwerthjkl45 <qwerthjkl45@gmail.com>
2018-04-12 17:12:57 +0200

    delete file

--- CV/Assignment_2/Part-1: Image Enhancement/gauss2D.m
+++
@@ -1,7 +0,0 @@
-function G = gauss2D( sigma , kernel_size )
-    %% solution
-    g = gauss1D(sigma, kernel_size);
-    G = g' * g;
-    G = G / sum(G);
-    
-end
--- CV/Assignment_2/Part-1: Image Enhancement/myPSNR.m
+++
@@ -1,7 +0,0 @@
-function [ PSNR ] = myPSNR( orig_image, approx_image )
-
-PSNR = psnr(orig_image, approx_image);
-
-end
-
-
